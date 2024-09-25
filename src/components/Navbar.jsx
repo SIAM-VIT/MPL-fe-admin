bc28a97a0f45f2fa66842808cd01e6c22934ce7e
rxshabN <nagwanirishab@gmail.com>
2024-09-26 00:09:38 +0530

    fix: homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './Navbar.css';  
-import logo from '../assets/MPLlogo.svg';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../assets/MPLlogo.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const nav = useNavigate();
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   function handleNavClick(e, path) {
     nav(path);
-    setIsMenuOpen(false); 
+    setIsMenuOpen(false);
   }
 
   function toggleMenu() {
@@ -17,20 +17,40 @@ export default function Navbar() {
   }
 
   return (
-    <div id="navBar" className='flex row space-between'>
-      <img src={logo} id='mplLogo' alt="MPL Logo" />
+    <div id="navBar" className="flex row space-between">
+      <img src={logo} id="mplLogo" alt="MPL Logo" />
       <div className="hamburgerMenu" onClick={toggleMenu}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <div id="menuBox" className={`flex row centerVH ${isMenuOpen ? 'open' : ''}`}>
-        <p className='menuItem' onClick={(e) => handleNavClick(e, '/home')}>Home</p>
-        <p className='menuItem' onClick={(e) => handleNavClick(e, '/leaderboard')}>Leaderboard</p>
-        <p className='menuItem' onClick={(e) => handleNavClick(e, '/treasure-hunt')}>Treasure Hunt</p>
+      <div
+        id="menuBox"
+        className={`flex row centerVH ${isMenuOpen ? "open" : ""}`}
+      >
+        <p className="menuItem" onClick={(e) => handleNavClick(e, "/home")}>
+          Home
+        </p>
+        <p
+          className="menuItem"
+          onClick={(e) => handleNavClick(e, "/leaderboard")}
+        >
+          Leaderboard
+        </p>
+        <p
+          className="menuItem"
+          onClick={(e) => handleNavClick(e, "/treasure-hunt")}
+        >
+          Treasure Hunt
+        </p>
         {/* <p className='menuItem' onClick={(e) => handleNavClick(e, '/jeopardy')}>Jeopardy</p>
         <p className='menuItem' onClick={(e) => handleNavClick(e, '/admin')}>Admin</p> */}
-        <button className='btn accentShade' onClick={(e) => handleNavClick(e, '/teams')}>Create Team</button>
+        <button
+          className="btn accentShade"
+          onClick={(e) => handleNavClick(e, "/teams")}
+        >
+          Create Team
+        </button>
       </div>
     </div>
   );

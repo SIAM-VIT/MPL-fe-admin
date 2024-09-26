88bd0f18fdfe651390f1621d6ad65a84358babf1
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-26 20:18:36 +0530

    fix: leaderboard

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar';
 
 export default function Leaderboard() {
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
-    fetch("https://mpl-be-p5xf.onrender.com/teams/getTeamsByScore")
+    fetch('https://mpl-be-p5xf.onrender.com/teams/getTeamsByScore')
       .then((response) => response.json())
       .then((data) => {
         const sortedTeams = data.sort((a, b) => b.score - a.score);
         setTeams(sortedTeams);
       })
-      .catch((error) => console.error("Error fetching teams:", error));
+      .catch((error) => console.error('Error fetching teams:', error));
   }, []);
 
   return (
     <div id="Container" className="flex column">
-      <Navbar widthClass="custom-width" />
+      <Navbar />
       <div id="bodyContainer">
         <div id="huntSection" className="flex column">
           <p className="titleHeadText">Leaderboard</p>
           <div id="clueSection" className="flex row">
+         
             <div id="leadertableSection">
-              <table
-                className="tbl"
-                id="leaderboardtable"
-                style={{ marginBottom: "20px" }}
-              >
+              <table className="tbl" id="leaderboardtable">
                 <thead>
                   <tr className="headerRow">
                     <th>Rank</th>
@@ -34,7 +31,7 @@ export default function Leaderboard() {
                     <th>Points</th>
                   </tr>
                 </thead>
-                <tbody style={{ backgroundColor: "white" }}>
+                <tbody>
                   {teams.map((team, index) => (
                     <tr key={team.team_id}>
                       <td className="bold">{index + 1}</td>
@@ -43,8 +40,7 @@ export default function Leaderboard() {
                     </tr>
                   ))}
                 </tbody>
-              </table>
-              <br />
+              </table><br></br>
             </div>
           </div>
         </div>
